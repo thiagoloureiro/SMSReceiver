28c537d273aaf41e3c22a6e64e1de0dcc718ca8a
Thiago Loureiro <thiagoguaru@outlook.com>
2017-11-18 17:04:40 +0100

    Fixed Tests

--- SMSReceiver.Test/GetMessageTest.cs
+++ SMSReceiver.Test/GetMessageTest.cs
@@ -4,20 +4,20 @@ namespace SMSReceiver.Test
 {
     public class GetMessageTest
     {
-        ////[Fact]
-        //public void GetMessage_Success()
-        //{
-        //    // Arrange
-        //    string phonenumber = "13433030421";
-        //    string prefix = "code";
-        //    bool clearprefix = true;
+        [Fact]
+        public void GetMessage_Success()
+        {
+            // Arrange
+            string phonenumber = "13433030421";
+            string prefix = "code";
+            bool clearprefix = true;
 
-        //    // Act
-        //    var objMessage = new RetrieveData(phonenumber, prefix, clearprefix);
-        //    var ret = objMessage.ReturnString();
+            // Act
+            var objMessage = new RetrieveData(phonenumber, prefix, clearprefix);
+            var ret = objMessage.ReturnString();
 
-        //    // Assert
-        //    Assert.True(ret.Length > 0);
-        //}
+            // Assert
+            Assert.True(ret.Length > 0);
+        }
     }
 }
